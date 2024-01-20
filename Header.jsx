7a42cb1097308dda8d984e1c1131e8cd92c82b5d
Gio <127239405+GioTini@users.noreply.github.com>
2024-01-20 14:24:25 -0300

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h3>Meu Chat</h3>
+      <button svg>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 128 512">
+          <path d="M64 360a56 56 0 1 0 0 112 56 56 0 1 0 0-112zm0-160a56 56 0 1 0 0 112 56 56 0 1 0 0-112zM120 96A56 56 0 1 0 8 96a56 56 0 1 0 112 0z" />
+        </svg>
+      </button>
+    </div>
+  );
+};
+
+export default Header;
